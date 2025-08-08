8559b7192b18d5d951fe0512f724b3a2d423c914
huygoodboy204 <huybg.tdconsulting@gmail.com>
2025-08-08 17:33:30 +0700

    Setup GitHub Pages deployment with gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist/spa",
+    outDir: ".",
     rollupOptions: {
       output: {
         manualChunks: undefined,
